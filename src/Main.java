3a0e7c218570babf8b436223876ad1c5ed4b8607
lsl775319@gmail.com <lsl775319@gmail.com>
2022-07-17 13:31:01 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_000;
+        int oneMilePrice = 20;
+        int Mail = ticketPrice / oneMilePrice;
+
+        System.out.println("Начислено " + Mail + " миль.");
+    }
+}
